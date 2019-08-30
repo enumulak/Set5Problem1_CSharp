ddac2403d886539cc51a5bf5675e92f4c0a6c28d
enumulak <enumula.kartik@outlook.com>
2019-08-30 10:35:20 +0530

    Final Working Solution

---
+++ Set5Problem1_CSharp/MessageProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Set5Problem1_CSharp
+{
+    public sealed class MessageProcessor
+    {
+        //Private Variables - Two Kingdom objects that represent the message sender and receiver
+        private Kingdom Sender;
+        private Kingdom Receiver;
+
+        //Static method to provide access to the Singleton Instance of this Class
+        public static MessageProcessor Instance { get; } = new MessageProcessor();
+
+
+        public void StartMessageProcessing(Kingdom sender, string msg, Kingdom receiver)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            ProcessMessageForKingdom(msg, Receiver);
+        }
+
+        private void ProcessMessageForKingdom(string msg, Kingdom r)
+        {
+            var found = r.GetEmblem().ToCharArray().Select(c => char.ToUpper(c)).Distinct().All(c => msg.ToUpper().Contains(c));
+
+            if (found)
+            {
+                //Transmit confirmation to the Receiver that its emblem was found in the Message, and then let it decide if it wants to be an Ally  of the sender or not...
+                r.ProcessAllegiance(found, Sender);
+            }
+        }
+    }
+}
---
+++ Set5Problem1_CSharp/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Set5Problem1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //All proceedings begin in Southeros...
+            Southeros.Instance.BeginShanConquest();
+
+            Console.ReadLine();
+
+        }
+    }
+}
